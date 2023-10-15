330fcdee451a52f5c5f08fd2adaaac725e3ffd6f
MrDeivi <dalfonsogcia@gmail.com>
2023-10-14 22:16:46 -0400

    chore: init
    Signed-off-by: MrDeivi <dalfonsogcia@gmail.com>

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const inputValue = ref()
+</script>
+
+<template>
+  <div class="">
+    Hola mundo
+
+    <TheCounter :initial="1" />
+    <TheInput v-model="inputValue" />
+  </div>
+  inputValue:{{ inputValue }}
+</template>

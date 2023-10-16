3173aebf71a90ff719e9b264a6ded2c7a8e14078
kriscarreno <krisjavao@gmail.com>
2023-10-16 16:25:12 -0400

    Cambios

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
-
+const emit = defineEmits(['toggle'])
+function toggleSideBar() {
+  emit('toggle')
+}
 </script>
 
 <template>
@@ -7,12 +10,12 @@
     <div class="px-3 py-3 lg:px-5 lg:pl-3">
       <div class="flex items-center justify-between">
         <div class="flex items-center justify-start">
-          <button id="toggleSidebar" aria-expanded="true" aria-controls="sidebar" class="mr-3 hidden cursor-pointer rounded p-2 text-gray-600 lg:inline hover:bg-gray-100 dark:text-gray-400 hover:text-gray-900 dark:hover:bg-gray-700 dark:hover:text-white">
+          <button id="toggleSidebar" aria-expanded="true" aria-controls="sidebar" class="mr-3 hidden cursor-pointer rounded p-2 text-gray-600 lg:inline hover:bg-gray-100 dark:text-gray-400 hover:text-gray-900 dark:hover:bg-gray-700 dark:hover:text-white" @click="toggleSideBar">
             <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
             </svg>
           </button>
-          <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar" class="mr-2 cursor-pointer rounded p-2 text-gray-600 lg:hidden focus:bg-gray-100 hover:bg-gray-100 dark:text-gray-400 hover:text-gray-900 focus:ring-2 focus:ring-gray-100 dark:focus:bg-gray-700 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700">
+          <button id="toggleSidebarMobile" aria-expanded="true" aria-controls="sidebar" class="mr-2 cursor-pointer rounded p-2 text-gray-600 lg:hidden focus:bg-gray-100 hover:bg-gray-100 dark:text-gray-400 hover:text-gray-900 focus:ring-2 focus:ring-gray-100 dark:focus:bg-gray-700 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700" @click="toggleSideBar">
             <svg id="toggleSidebarMobileHamburger" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
             </svg>
--- src/components/sidebar/Sidebar.vue
+++ src/components/sidebar/Sidebar.vue
@@ -22,19 +22,19 @@
             <li>
               <a href="./index.html" class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
                 <svg class="h-6 w-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" /><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" /></svg>
-                <span class="ml-3" sidebar-toggle-item="">Dashboard</span>
+                <span class="ml-3" sidebar-toggle-item="">Donaciones</span>
               </a>
             </li>
             <li>
               <a href="./kanban.html" class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
                 <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap" sidebar-toggle-item="">Kanban</span>
+                <span class="ml-3 flex-1 whitespace-nowrap" sidebar-toggle-item="">Información</span>
               </a>
             </li>
             <li>
               <a href="./mailing/inbox.html" class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
                 <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z" /><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap" sidebar-toggle-item="">Inbox</span>
+                <span class="ml-3 flex-1 whitespace-nowrap" sidebar-toggle-item="">Solicitudes</span>
                 <span class="text-primary-800 bg-primary-100 ml-3 h-5 w-5 inline-flex items-center justify-center rounded-full p-1 text-sm font-medium" sidebar-toggle-item="">3</span>
               </a>
             </li>
@@ -42,86 +42,8 @@
             <li>
               <button type="button" class="group w-full flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-ecommerce" data-collapse-toggle="dropdown-ecommerce" aria-expanded="false">
                 <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap text-left" sidebar-toggle-item="">E-commerce</span>
-                <svg sidebar-toggle-item="" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+                <span class="ml-3 flex-1 whitespace-nowrap text-left" sidebar-toggle-item="">Centros de Donación</span>
               </button>
-              <ul id="dropdown-ecommerce" class="hidden py-2 space-y-2">
-                <li>
-                  <a href="./e-commerce/products.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Products</span><span class="hidden">P</span></a>
-                </li>
-                <li>
-                  <a href="./e-commerce/billing.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Billing</span><span class="hidden">B</span></a>
-                </li>
-                <li>
-                  <a href="./e-commerce/invoice.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Invoice</span><span class="hidden">I</span></a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type="button" class="group w-full flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-users" data-collapse-toggle="dropdown-users" aria-expanded="true">
-                <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap text-left" sidebar-toggle-item="">Users</span>
-                <svg sidebar-toggle-item="" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-              </button>
-              <ul id="dropdown-users" class="py-2 space-y-2">
-                <li>
-                  <a href="./users/list.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Users list</span><span class="hidden">U</span></a>
-                </li>
-                <li>
-                  <a href="./users/profile.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Profile</span><span class="hidden">P</span></a>
-                </li>
-                <li>
-                  <a href="./users/feed.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Feed</span><span class="hidden">F</span></a>
-                </li>
-                <li>
-                  <a href="./users/settings.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Settings</span><span class="hidden">S</span></a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type="button" class="group w-full flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-pages" data-collapse-toggle="dropdown-pages" aria-expanded="true">
-                <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm2 10a1 1 0 10-2 0v3a1 1 0 102 0v-3zm2-3a1 1 0 011 1v5a1 1 0 11-2 0v-5a1 1 0 011-1zm4-1a1 1 0 10-2 0v7a1 1 0 102 0V8z" clip-rule="evenodd" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap text-left" sidebar-toggle-item="">Pages</span>
-                <svg sidebar-toggle-item="" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-              </button>
-              <ul id="dropdown-pages" class="py-2 space-y-2">
-                <li>
-                  <a href="./pages/pricing.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Pricing</span><span class="hidden">P</span></a>
-                </li>
-                <li>
-                  <a href="./pages/maintenance.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Maintenance</span><span class="hidden">M</span></a>
-                </li>
-                <li>
-                  <a href="./pages/404.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>404 not found</span><span class="hidden">4</span></a>
-                </li>
-                <li>
-                  <a href="./pages/500.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>500 server error</span><span class="hidden">5</span></a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <button type="button" class="group w-full flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700" aria-controls="dropdown-auth" data-collapse-toggle="dropdown-auth" aria-expanded="false">
-                <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" /></svg>
-                <span class="ml-3 flex-1 whitespace-nowrap text-left" sidebar-toggle-item="">Authentication</span>
-                <svg sidebar-toggle-item="" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
-              </button>
-              <ul id="dropdown-auth" class="hidden py-2 space-y-2">
-                <li>
-                  <a href="./authentication/sign-in.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Sign in</span><span class="hidden">S</span></a>
-                </li>
-                <li>
-                  <a href="./authentication/sign-up.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Sign up</span><span class="hidden">S</span></a>
-                </li>
-                <li>
-                  <a href="./authentication/forgot-password.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Forgot password</span><span class="hidden">F</span></a>
-                </li>
-                <li>
-                  <a href="./authentication/reset-password.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Reset password</span><span class="hidden">R</span></a>
-                </li>
-                <li>
-                  <a href="./authentication/profile-lock.html" class="group flex items-center rounded-lg p-2 pl-11 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"><span>Profile lock</span><span class="hidden">P</span></a>
-                </li>
-              </ul>
             </li>
           </ul>
           <div class="pt-2 space-y-2">
@@ -131,11 +53,11 @@
             </a>
             <a href="https://flowbite.com/docs/components/alerts/" target="_blank" class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
               <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" /></svg>
-              <span class="ml-3" sidebar-toggle-item="">Components</span>
+              <span class="ml-3" sidebar-toggle-item="">Perfil</span>
             </a>
             <a href="https://github.com/themesberg/flowbite/issues" target="_blank" class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">
               <svg class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-2 0c0 .993-.241 1.929-.668 2.754l-1.524-1.525a3.997 3.997 0 00.078-2.183l1.562-1.562C15.802 8.249 16 9.1 16 10zm-5.165 3.913l1.58 1.58A5.98 5.98 0 0110 16a5.976 5.976 0 01-2.516-.552l1.562-1.562a4.006 4.006 0 001.789.027zm-4.677-2.796a4.002 4.002 0 01-.041-2.08l-.08.08-1.53-1.533A5.98 5.98 0 004 10c0 .954.223 1.856.619 2.657l1.54-1.54zm1.088-6.45A5.974 5.974 0 0110 4c.954 0 1.856.223 2.657.619l-1.54 1.54a4.002 4.002 0 00-2.346.033L7.246 4.668zM12 10a2 2 0 11-4 0 2 2 0 014 0z" clip-rule="evenodd" /></svg>
-              <span class="ml-3" sidebar-toggle-item="">Help</span>
+              <span class="ml-3" sidebar-toggle-item="">Ayuda</span>
             </a>
           </div>
         </div>
